0eae5a36f239cf2a501d6c907d6745c49440f203
Zikrullo <zikrullogaming@gmail.com>
2024-09-16 21:01:15 +0500

    t4: Parse lenta.ru main current news and send link (only LINK) to telegram channel

--- app/Console/Commands/ParseNews.php
+++ app/Console/Commands/ParseNews.php
@@ -3,7 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Log;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 class ParseNews extends Command
 {
@@ -19,14 +20,94 @@ class ParseNews extends Command
      *
      * @var string
      */
-    protected $description = 'News parsing and logging';
+    protected $description = 'News parsing and logging using Guzzle and Symfony DOM Crawler';
+
+    /**
+     * Telegram Bot Token.
+     *
+     * @var string
+     */
+    protected $telegramBotToken = '7296354081:AAFMmqojjLISE3FA6c4jcmofNc6Fl_nDZN4';
+
+    /**
+     * Telegram Channel ID.
+     *
+     * @var string
+     */
+    protected $telegramChannelId = '-1002445745323';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        Log::info('Задача parse:news выполнена успешно.');
-        $this->info('Задача parse:news выполнена и запись добавлена в лог.');
+        $client = new Client();
+        $url = 'https://lenta.ru/';
+        $response = $client->get($url);
+        $html = (string) $response->getBody();
+        $crawler = new Crawler($html);
+
+        $newsItems = $crawler->filter('a.card-big._topnews._news');
+
+        if ($newsItems->count() === 0) {
+            $this->error('Новостные блоки не найдены');
+            return;
+        }
+
+        $newsData = [];
+
+        $newsItems->each(function (Crawler $node) use ($url, &$newsData) {
+            try {
+                // Получаем заголовок новости
+                $titleNode = $node->filter('.card-big__title');
+                if ($titleNode->count() === 0) {
+                    throw new \Exception('Заголовок не найден');
+                }
+                $title = $titleNode->text();
+
+                // Получаем ссылку на новость
+                $link = 'https://lenta.ru' . $node->attr('href');
+                $this->sendToTelegram($link);
+
+                // Сохраняем данные в массив
+                $newsData[] = [
+                    'title' => $title,
+                    'link' => $link,
+                    'source' => $url, // Исходный адрес сайта
+                ];
+
+                // Выводим ссылку в консоль
+                $this->info("Заголовок: $title");
+                $this->info("Ссылка: $link");
+                $this->info("Источник: $url");
+                $this->info(str_repeat('=', 50)); // Разделитель для удобства чтения
+
+            } catch (\Exception $e) {
+                $this->error('Ошибка: ' . $e->getMessage());
+            }
+        });
+    }
+
+    /**
+     * Отправка сообщения в Telegram.
+     *
+     * @param string $message
+     */
+    protected function sendToTelegram(string $message)
+    {
+        $client = new Client();
+        $url = "https://api.telegram.org/bot{$this->telegramBotToken}/sendMessage";
+
+        try {
+            $client->post($url, [
+                'json' => [
+                    'chat_id' => $this->telegramChannelId,
+                    'text' => $message,
+                    'parse_mode' => 'HTML',
+                ],
+            ]);
+        } catch (\Exception $e) {
+            $this->error('Не удалось отправить сообщение в Telegram: ' . $e->getMessage());
+        }
     }
 }
